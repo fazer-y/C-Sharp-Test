e4da3c1bb78f1e81bc0b776804cd92046c28fd6c
Fazer-y <77623899+fazer-y@users.noreply.github.com>
2022-06-10 15:19:59 +0800

    添加项目文件。

---
+++ LinPengFeiCon/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinPengFeiCon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+        }
+        // 前八章，不考第七章
+        // 没星号的或有星号但是讲了的
+        // linq
+        // 不考winform
+    }
+
+    public class MyClass
+    {
+        public MyClass()
+        {
+            Console.Write("请输入待排序字符串：");
+            string str = Console.ReadLine();
+            List<char> l = str.ToList();
+            l.Sort();
+            foreach (char c1 in l)
+            {
+                Console.WriteLine(c1);
+            }
+            Console.Write("请输入待查找字符：");
+            char c = (char)Console.Read();
+            if (l.Contains(c))
+                Console.WriteLine("是！");
+            else
+                Console.WriteLine("否！");
+            while (true)
+            {
+                Console.Write("输入exit以退出系统：");
+                string exStr = Console.ReadLine();
+                if (exStr == "exit")
+                    return;
+            }
+        }
+    }
+}
---
+++ LinPengFeiWPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LinPengFeiWPF
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static void ExecuteConsoleApp(string arg)
+        {
+            // exe路径
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            // 完整路径
+            path = path.Replace(@"WPF", @"Con");
+            // 运行ConsoleApp
+            System.Diagnostics.Process.Start(path, arg);
+        }
+    }
+}
